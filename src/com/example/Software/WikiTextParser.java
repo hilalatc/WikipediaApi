9c9e69c3ab987e3b2bcf3194c5a0c3433d0cbcfd
Hilal Atıcı <hilalatc@gmail.com>
2013-11-20 17:15:46 +0200

    Infobox-WikiTextParser

---
+++ src/com/example/Software/WikiTextParser.java
@@ -0,0 +1,11 @@
+package com.example.Software;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Hilal
+ * Date: 20.11.2013
+ * Time: 17:15
+ * To change this template use File | Settings | File Templates.
+ */
+public class WikiTextParser {
+}
