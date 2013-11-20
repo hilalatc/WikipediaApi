307c2658cbeffaf309636ec043a8f6f786d63898
Hilal Atıcı <hilalatc@gmail.com>
2013-11-20 18:34:06 +0200

    InfoBox-WikiTextParser

--- src/com/example/Software/WikiTextParser.java
+++ src/com/example/Software/WikiTextParser.java
@@ -3,7 +3,7 @@ package com.example.Software;
 /**
  * Created with IntelliJ IDEA.
  * User: Hilal
- * Date: 20.11.2013
+ * Date: 24.10.2013
  * Time: 17:15
  * To change this template use File | Settings | File Templates.
  */
