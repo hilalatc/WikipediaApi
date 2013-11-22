a46fb019707831e01922d286f3cc53edaade9430
Hilal Atıcı <hilalatc@gmail.com>
2013-11-22 20:23:23 +0200

    Wikipedia Api

--- src/com/example/Software/AnaMain.java
+++ src/com/example/Software/AnaMain.java
@@ -3,7 +3,7 @@ package com.example.Software;
 /**
  * Created with IntelliJ IDEA.
  * User: Hilal
- * Date: 22.11.2013
+ * Date: 24.10.2013
  * Time: 20:19
  * To change this template use File | Settings | File Templates.
  */
