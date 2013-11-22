d885faaca0c4b67cd46367c0a59828485f7cd7ee
Hilal Atıcı <hilalatc@gmail.com>
2013-11-22 20:20:48 +0200

    Wikipedia Api

---
+++ src/com/example/Software/ListeEkran.java
@@ -0,0 +1,11 @@
+package com.example.Software;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Hilal
+ * Date: 22.11.2013
+ * Time: 20:20
+ * To change this template use File | Settings | File Templates.
+ */
+public class ListeEkran {
+}
