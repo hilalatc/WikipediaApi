f227a396c6a0a5b920eddcaf3b243f91678118ec
Hilal Atıcı <hilalatc@gmail.com>
2013-11-23 17:50:54 +0200

    Wikipedia Api

--- src/com/example/Software/AnaMain.java
+++ src/com/example/Software/AnaMain.java
@@ -1,5 +1,12 @@
 package com.example.Software;
 
+import android.app.Activity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Hilal
@@ -7,5 +14,10 @@ package com.example.Software;
  * Time: 20:19
  * To change this template use File | Settings | File Templates.
  */
-public class AnaMain {
+public class AnaMain extends Activity {
+    TextView tvname;
+    ImageView image;
+
+    HashMap<String, String> map;
+    ArrayList<HashMap<String, String>> List;
 }
