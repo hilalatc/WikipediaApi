59fb68f9eee31a625b1a81b98e16ee85e40e5073
Hilal Atıcı <hilalatc@gmail.com>
2013-11-24 19:00:26 +0200

    Wikipedia Api

--- src/com/example/Software/AnaMain.java
+++ src/com/example/Software/AnaMain.java
@@ -31,5 +31,8 @@ public class AnaMain extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.anamain);
+
+        tvname=(TextView)findViewById(R.id.textView);
+        image=(ImageView)findViewById(R.id.imageView);
     }
 }
