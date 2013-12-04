c688ada33134bfea66f1e95209d62766dbebb9d8
Hilal Atıcı <hilalatc@gmail.com>
2013-12-04 17:38:50 +0200

    MyActivity (ilk ekran)

--- src/com/example/Software/MyActivity.java
+++ src/com/example/Software/MyActivity.java
@@ -1,6 +1,7 @@
 package com.example.Software;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MyActivity extends Activity {
@@ -11,5 +12,36 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        // Thread hazirlaniyor
+        Thread thread = new Thread() {
+
+            @Override
+            public void run() {
+
+                try {
+                    synchronized (this) {
+                        // Uygulama 4 saniye ayni ekranda bekliyor
+                        wait(1000);
+                    }
+                } catch (InterruptedException e) {
+
+                    // Hata yonetimi
+
+                } finally {
+
+                    finish();
+
+                    // Yeni acılmak istenen Intent
+                    Intent intent = new Intent();
+                    intent.setClass(getApplicationContext(), ListeEkran.class);
+                    startActivity(intent);
+
+                }
+
+            }
+        };
+
+        // Thread ba�lat�l�yor
+        thread.start();
     }
 }
